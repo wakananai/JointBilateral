b3cd2266192098c65fbf43756811ada4eef8e273
rowanho <rowanho45@gmail.com>
2018-11-13 13:36:25 +0000

    joint bilateral filter

--- ip.py
+++ ip.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+import os
 
 
 #makes the dimensions of our mask in range for corners
@@ -51,13 +52,37 @@ def bilateralPix(img,d,x,y,stdevC,stdevD):
 				distanceGauss = gaussian(math.sqrt((x-a)**2 + (y-b)**2),stdevD) 
 				total[c] += (colorGauss * distanceGauss * img[a,b,c])
 				totalDivisor[c] += colorGauss * distanceGauss 
-	#print(total/totalDivisor)
-	result = np.asarray(total/totalDivisor, dtype=np.uint8)	#
-	#print(result)
-	#print('old')
-	#print(img[x,y])
+	return total/totalDivisor
 
-	return result
+#the joint bilateral filter for a pixel
+def jointBilateralPix(flash,noFlash,d,x,y,stdevC,stdevD):
+	#don't use indexes that are out of range
+	#find ranges of mask
+	if d % 2 == 0:
+		low_x = x - (d//2)
+		low_y = y - (d//2)
+
+	else:
+		low_x = x - (d//2 +1)
+		low_y = y - (d//2 +1)
+	high_x = x + (d//2)
+	high_y = y + (d//2)
+	low_x,low_y,high_x,high_y  = MakeInRange(flash,low_x,low_y,high_x,high_y)
+
+	#find the sum of the gaussian functions
+	total= np.zeros(3)
+	totalDivisor= np.zeros(3)
+	#print('range: ' + str(high_x - low_x))
+	#print('range: ' + str(high_y - low_y))
+
+	for a in range(low_x,high_x + 1):
+		for b in range(low_y,high_y +1):
+			for c in range(3): # do this for each color
+				colorGauss = gaussian(abs(flash.item(x,y,c) - flash.item(a,b,c)),stdevC)
+				distanceGauss = gaussian(math.sqrt((x-a)**2 + (y-b)**2),stdevD) 
+				total[c] += (colorGauss * distanceGauss * noFlash[a,b,c])
+				totalDivisor[c] += colorGauss * distanceGauss 
+	return total/totalDivisor	
 
 #full bilateral filter
 def bilateral(img,d,stdevC,stdevD):
@@ -76,22 +101,27 @@ def bilateralInbuilt(img,d,stdevC,stdevD):
 	print(result)	
 	return result
 
-def jointBilateral(flash,noFlash,d):
-	#loop through flash image
+def jointBilateral(flash,noFlash,d,stdevC,stdevD):
+	newImg = np.zeros((flash.shape[0],flash.shape[1],3),dtype = int)
 	for i in range(flash.shape[0]):
 		for j in range(flash.shape[1]):
-			pass
-	print(flash)
-	img = cv2.bilateralFilter(flash,5,100,100)
-	#loop through indexes
-	return img
+			newImg[i,j] = jointBilateralPix(flash,noFlash,d,i,j,stdevC,stdevD)
+	result = np.asarray(newImg, dtype=np.uint8)	
+
+	return result
+
 
 def main():
 	img = cv2.imread('./meme.jpg', cv2.IMREAD_COLOR);
 	noFlashImg = cv2.imread('./test3a.jpg', cv2.IMREAD_COLOR);
 	flashImg = cv2.imread('./test3b.jpg', cv2.IMREAD_COLOR);
 	#jointImg = jointBilateral(flashImg,noFlashImg)
-	filteredImg = bilateral(img,5,50,50)
+	stdev1 = 10
+	stdev2 = 10
+	filteredImg = jointBilateral(flashImg,noFlashImg,5,stdev1,stdev2)
+	name = 'filteredimg' + str(stdev1) + ' ' + str(stdev2) +'.png'
+	path = 'C:/Users/rowan/Documents/uni_year_2/image processing/coursework'
+	cv2.imwrite(os.path.join(path,name),filteredImg)
 	if not filteredImg is None:
 	    cv2.namedWindow('bilateral filter');
 	    # set a loop control flag
