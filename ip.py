f34a3525647adc620d43d71b2108a14c73691b8a
rowanho <rowanho45@gmail.com>
2018-11-23 15:55:54 +0000

    vectorized

--- ip.py
+++ ip.py
@@ -6,27 +6,38 @@ sqrt2pi = math.sqrt(2 * math.pi )
 
 #makes the dimensions of our mask in range for corners
 def MakeInRange(img,low_x,low_y,high_x,high_y):
+	add_x=add_y=cut_x=cut_y = 0
 	if low_x < 0:
+		add_x = abs(low_x)
 		low_x = 0
-	if low_y < 0 :
+	if low_y < 0:
+		add_y = abs(low_y)
 		low_y = 0
-	if high_x > (img.shape[0] -1):
-		high_x = img.shape[0] -1
 
+	if high_x > (img.shape[0] -1):
+		cut_x = high_x - (img.shape[0] -1) 
+		high_x = img.shape[0] -1
 	if high_y > img.shape[1] - 1:
+		cut_y = high_y - (img.shape[1] -1) 
 		high_y = img.shape[1] -1
-	return low_x,low_y,high_x,high_y
+
+	return add_x,add_y,cut_x,cut_y,low_x,low_y,high_x,high_y
 	
 #the actual gaussian function
-def gaussian(number,stdev):
-	power = -0.5 * ((number/stdev)**2)
+def gaussian(mask,stdev):
+	power = -0.5 * (np.square(mask/stdev))
 	divisor = stdev * sqrt2pi
-	result = math.exp(power)/divisor
-
+	result = np.exp(power)/divisor
 	return result
 
+def get2dKernel(d,stdev):
+	k=  cv2.getGaussianKernel(d,stdev)
+	x = np.stack((k,)*3, axis=-1)
+	y = x.reshape((1,d,3))
+	b = np.ones((d,d,3))
+	return x * y * b
 #the joint bilateral filter for a pixel
-def jointBilateralPix(flash,noFlash,d,x,y,gaussianColorDict,gaussianDistDict):
+def jointBilateralPix(flash,noFlash,d,x,y,stdevC,stdevD):
 	#find ranges of mask
 	if d % 2 == 0:
 		low_x = x - (d//2)
@@ -36,47 +47,39 @@ def jointBilateralPix(flash,noFlash,d,x,y,gaussianColorDict,gaussianDistDict):
 		low_y = y - (d//2 +1)
 	high_x = x + (d//2)
 	high_y = y + (d//2)
-	low_x,low_y,high_x,high_y  = MakeInRange(flash,low_x,low_y,high_x,high_y)
+	add_x,add_y,cut_x,cut_y,low_x,low_y,high_x,high_y  = MakeInRange(flash,low_x,low_y,high_x,high_y)
 	#find the sum of the gaussian functions
 	total= np.zeros(3) 
 	totalDivisor= np.zeros(3)
 	colorGauss = np.zeros(3)
 	colour = flash[x,y]
-	for a in range(low_x,high_x + 1):
-		for b in range(low_y,high_y +1):
-			distanceGauss = gaussianDistDict[math.sqrt((x-a)**2 + (y-b)**2)] 
-			for c in range(3): # do this for each color
-				colorGauss[c] = gaussianColorDict[abs(colour[c] - flash.item(a,b,c))]
-			total += (colorGauss * distanceGauss * noFlash[a,b])
-			totalDivisor += colorGauss * distanceGauss 
-	return total/totalDivisor
+ 	#added 
+	# get gaussian masks
+	colourMask = gaussian(flash[low_x:high_x,low_y:high_y] - colour,stdevC)
+	distanceMask = get2dKernel(d,stdevD)[add_x:d-cut_x,add_y:d-cut_y]
+	mask =  distanceMask * colourMask
+	total = np.sum(mask * noFlash[low_x:high_x,low_y:high_y], axis = (0,1))
+	totalDiv = np.sum(mask, axis = (0,1))
+	return total/totalDiv
 
 def jointBilateral(flash,noFlash,d,stdevC,stdevD):
 	#precompute guassian values in dict
-	gaussianColorDict = {}
-	for i in range(256):
-		gaussianColorDict[i] = gaussian(i,stdevC)
-	gaussianDistDict = {}
-	for i in range(d//2 + 2):
-		for j in range(d//2 + 2):
-			pythag = math.sqrt(i**2 + j**2)
-			gaussianDistDict[pythag] = gaussian(pythag,stdevD)
 	newImg = np.zeros((flash.shape[0],flash.shape[1],3),dtype = int)
 	for i in range(flash.shape[0]):
 		for j in range(flash.shape[1]):
-			newImg[i,j] = jointBilateralPix(flash,noFlash,d,i,j,gaussianColorDict,gaussianDistDict)
+			newImg[i,j] = jointBilateralPix(flash,noFlash,d,i,j,stdevC,stdevD)
 	result = np.asarray(newImg, dtype=np.uint8)	
 
 	return result
 
 def main():
-	imgname = 'join' 
-	#img = cv2.imread('./' + imgname, cv2.IMREAD_COLOR);
-	noFlashImg = cv2.imread('./test3a.jpg', cv2.IMREAD_COLOR);
-	flashImg = cv2.imread('./test3b.jpg', cv2.IMREAD_COLOR);
+	imgname = 'joint' 
+	#img = cv2.imread('./' + imgname, cv2.IMREAD_COLOR)
+	noFlashImg = cv2.imread('./test3a.jpg', cv2.IMREAD_COLOR)
+	flashImg = cv2.imread('./test3b.jpg', cv2.IMREAD_COLOR)
 
-	stdev1 =100
-	stdev2 = 100
+	stdev1 =10
+	stdev2 = 10
 	diam = 9
 	filteredImg = jointBilateral(flashImg,noFlashImg,diam,stdev1,stdev2)
 	#filteredImg = cv2.bilateralFilter(img,diam,stdev1,stdev2)
@@ -95,9 +98,21 @@ def main():
 	else:
 		print("No image file successfully loaded.")
 
-# ... and finally close all windows
-
 cv2.destroyAllWindows();
 
+def test():
+	imgname = 'test2.png' 
+	img = cv2.imread('./' + imgname, cv2.IMREAD_COLOR)
+	diam = 9
+	stdev1 = 5
+	stdev2 = 10
+	filteredImg = (cv2.bilateralFilter(img,diam,stdev1,stdev2))
+	difference = (cv2.bilateralFilter(img,diam,stdev1,stdev2)) - img 
+	name = 'diff_' + imgname + str(diam) + '_' + str(stdev1) + '_' + str(stdev2) +'.png'
+	path = 'C:/Users/rowan/Documents/uni_year_2/image processing/coursework'
+	cv2.imwrite(os.path.join(path,name),difference)
+# ... and finally close all windows
 
 main()
+
+
